35eb7f5fe8ab2bba43676441d2859208fb436598
Abhishek Malakar <abhishekmalakar024@gmail.com>
2025-11-25 21:43:54 +0530

    Problem 60

--- Hard/Problem_60.py
+++ Hard/Problem_60.py
@@ -1,1 +1,14 @@
-git add .
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        k -= 1
+        nums = [i + 1 for i in range(n)]
+        result = []
+        
+        for i in range(n - 1, -1, -1):
+            fact = math.factorial(i)
+            index = k // fact
+            k %= fact
+            result.append(str(nums[index]))
+            nums.pop(index)
+            
+        return "".join(result)
