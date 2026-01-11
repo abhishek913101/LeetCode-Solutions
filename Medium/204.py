1f5831b14ed2a94f7d1a3e7475ecf9351ef6e4e8
Abhishek Malakar <abhishekmalakar024@gmail.com>
2026-01-11 15:50:07 +0530

    '204'

---
+++ Medium/204.py
@@ -0,0 +1,14 @@
+if n <= 2:
+    return 0
+isPrime = [True] * n
+isPrime[0] = isPrime[1] = False
+
+for i in range(2, int(n**0.5) + 1):
+    if isPrime[i]:
+        for j in range(i * i, n, i):
+            isPrime[j] = False
+count = 0
+for i in range(n):
+    if isPrime[i]:
+        count += 1
+return count
